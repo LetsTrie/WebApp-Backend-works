b37fc0dd5fadea94ca0692d2027797f808714c25
letstrie <sakibkhan111296@gmail.com>
2019-03-06 19:36:35 +0600

    Sign-UP details info upload done in firebase realtime database

--- create_post.js
+++ create_post.js
@@ -1,20 +1,21 @@
 function post_now() {
-  var post_author = firebase.auth().currentUser;
+  var post_author_email = firebase.auth().currentUser.email;
   var post_title = document.getElementById("title_ID").value;
   var post_description = document.getElementById("description_ID").value;
   var post_tag = document.getElementById("tag_ID").value;
   var post_readingTime = document.getElementById("minute_ID").value;
   var post_postTime = new Date();
 
-  // var newPostKey = firebase.database().ref().child('UserPost').push().key;
-  
+  window.alert(post_author_email + " " + post_description + " " + post_title + " " + post_tag);
+
   // //it is not working after that..... May be I coded in wrong way. 
   // //I wanted to branch these informations. post's are in "UserPost" branch. user's info is in "UserInfo" branch.
-  
-  // firebase.database().ref('UserPost').child(newPostKey).set({
-  //   Author  : post_author,
-  //   Title   : post_title,
-  //   Description : post_description,
-  // });
+  var newPostKey = firebase.database().ref().child('UserPost').push().key;
+  firebase.database().ref('/UserPost/' + newPostKey).set({
+    Author_email    : post_author_email,
+    Title           : post_title,
+    Description     : post_description
+  });
+  // it's not going further...
   window.location = "homepage.html";
 }
