7ee14450cdd0f0f94afe5f60500d780162718c8b
letstrie <sakibkhan111296@gmail.com>
2019-03-07 18:15:09 +0600

    update

--- sell_book.js
+++ sell_book.js
@@ -8,12 +8,27 @@ function sell_now() {
   var newPostKey = firebase.database().ref().child('BookSellPost').push().key;
 
   firebase.database().ref('/BookSellPost/' + newPostKey).set({
-    Seller_Name     		: book_seller_Name,
+    Seller_Name     			: book_seller_Name,
     Book_Title           	: book_title,
     Book_Description     	: book_description,
     Book_Tag             	: book_tag,
-    PostTime        		: book_postTime
+    PostTime        			: book_postTime
   }).then(function() {
     window.location = "homepage.html";
   });
+}
+
+
+function sell_book_page_selector()  {
+	try {
+		var user = firebase.auth().currentUser;
+		if(user != null) {
+			window.location = "CreateNewPost.html";
+		} else {
+			window.location = "sign_in.html";
+		}
+	}
+	catch (err) {
+			window.location = "sign_in.html";
+	} 
 }
